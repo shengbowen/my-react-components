13d64409f8ac0a8582e465214f78d8750e48df30
bruce.sheng <shengbowen@meizu.com>
2018-04-22 20:30:58 +0800

    递归Caspanel

--- src/components/cascade/caspanel.jsx
+++ src/components/cascade/caspanel.jsx
@@ -5,19 +5,22 @@ import Casitem from './casitem';
 class Caspanel extends Component {
   constructor(props) {
     super(props);
+    console.log(props);
     this.state = {
-      sublist: []
+      sublist: props.data[0].children
     };
   }
 
   render() {
     const { data } = this.props;
+    const { sublist } = this.state;
 
     return (
       <div className="caspanel-wrap">
         <ul className="caspanel-menu">
           { data.map((item) => <Casitem data={ item }/>) }
         </ul>
+        { sublist && sublist.length && <Caspanel data={sublist} />}
       </div>
     );
   }
