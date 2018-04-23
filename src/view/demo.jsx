d438f73f6aa3c9ba3d543b70c3a7c473f4b4fe21
bruce.sheng <shengbowen@meizu.com>
2018-04-23 20:53:10 +0800

    add form

--- src/view/demo.jsx
+++ src/view/demo.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import Cascade from '../components/cascade/cascade';
 
+import AddForm from './form';
+import mockData from '../components/cascade/mock';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(value) {
+    console.log(value)
+  }
+
   render() {
     return (
-      <Cascade separator="/" />
+      <div>
+        <AddForm data={this.props.data}/>
+        <Cascade separator="/" />
+      </div>
     );
   }
+}
+
+Component.defaultProps = {
+  data: mockData,
 }
 
 export default App;
---
+++ src/view/form.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+
+import { Select, Form, Icon, Input, Button } from 'antd';
+
+const Option = Select.Option;
+const FormItem = Form.Item;
+
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+
+class AddForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedCity: this.props.data[0].value,
+    };
+  }
+
+  componentDidMount() {
+    // To disabled submit button at the beginning.
+    this.props.form.validateFields();
+    console.log(this.props.form);
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      selectedCity: value
+    })
+  }
+
+  render() {
+    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+    const options = this.props.data;
+    // Only show error after a field is touched.
+    const valueError = isFieldTouched('value') && getFieldError('value');
+    const labelError = isFieldTouched('label') && getFieldError('label');
+    return (
+      <Form layout="inline" onSubmit={this.handleSubmit}>
+        <FormItem >
+          <Select defaultValue={this.state.selectedCity} style={{ width: 120 }} onChange={this.handleChange}>
+            {
+              options.map(item => {
+                return (
+                  <Option value={ item.value } key={ item.value }>{ item.label }</Option>
+                )
+              })
+            }
+          </Select>
+        </FormItem>
+        <FormItem
+          validateStatus={valueError ? 'error' : ''}
+          help={valueError || ''}
+        >
+          {getFieldDecorator('value', {
+            rules: [{ required: true, message: '请输入value' }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Value" />
+          )}
+        </FormItem>
+        <FormItem
+          validateStatus={labelError ? 'error' : ''}
+          help={labelError || ''}
+        >
+          {getFieldDecorator('label', {
+            rules: [{ required: true, message: '请输入label' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Label" />
+          )}
+        </FormItem>
+        <FormItem>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={hasErrors(getFieldsError())}
+          >
+            Add
+          </Button>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+const WrappedAddForm = Form.create({})(AddForm);
+export default WrappedAddForm;
